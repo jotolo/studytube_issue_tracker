11a742d6493382768f07125d10c0f0877dd42ce8
Jorge T <jotolo7@gmail.com>
2018-09-22 13:30:15 +0200

    Fixing ability file for authorization.

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,39 +2,24 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    # Define abilities for the passed in user here. For example:
-    #
        user ||= User.new # guest user (not logged in)
        if user.is_manager?
+         # be able to see the list of all issues
          can :index, Issue
+         # be able to assign an issue to only yourself and only if it is not already assigned to somebody else
+         # be able to unassign an issue from yourself
          can :update, Issue, manager_id: [user.id, nil]
          can :show, Issue
        elsif user.is_user?
+         # see the list of only your issues (most recent at the top)
          can :index, Issue
          # be able to create/update/delete only your issues
          can :create, Issue, user_id: user.id
          can :update, Issue, user_id: user.id
          can :destroy, Issue, user_id: user.id
+         # see only your issues
          can :show, Issue, user_id: user.id
 
        end
-    #
-    # The first argument to `can` is the action you are giving the user
-    # permission to do.
-    # If you pass :manage it will apply to every action. Other common actions
-    # here are :read, :create, :update and :destroy.
-    #
-    # The second argument is the resource the user can perform the action on.
-    # If you pass :all it will apply to every resource. Otherwise pass a Ruby
-    # class of the resource.
-    #
-    # The third argument is an optional hash of conditions to further filter the
-    # objects.
-    # For example, here the user can only update published articles.
-    #
-    #   can :update, Article, :published => true
-    #
-    # See the wiki for details:
-    # https://github.com/CanCanCommunity/cancancan/wiki/Defining-Abilities
   end
 end
